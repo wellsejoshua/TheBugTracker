2ff8e2fd5259b61552445d4b1de667ecf116ed33
wellsejoshua <wellsejoshua@outlook.com>
2022-07-08 09:54:37 -0500

    Models: Company, Invite, and Notification Created; BTUser edited for join table

---
+++ TheBugTracker/Models/Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Company Name")]
+        public string  Name { get; set; }
+
+        [DisplayName("Company Description")]
+        public string Description { get; set; }
+
+        //Navigation Properties
+        public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
+        public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
+
+    }
+}
