2f9caf53ef691fba37614137bef02f385c7a91eb
wellsejoshua <wellsejoshua@outlook.com>
2022-07-08 08:28:21 -0500

    Models: Project and Project Priority Created

---
+++ TheBugTracker/Models/Project.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Company")]
+        public int? CompanyId { get; set; }
+
+        [DisplayName("Priority")]
+        public int? ProjectPriorityId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Project Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [DisplayName("Start Date")]
+        public DateTimeOffset StartDate { get; set; }
+
+        [DisplayName("End Date")]
+        public DateTimeOffset EndDate { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile ImageFormFile { get; set; }
+
+        [DisplayName("File Name")]
+        public string ImageFileName { get; set; }
+
+        public byte[] ImageFileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string ImageFileContentType { get; set; }
+
+        [DisplayName("Archived")]
+        public bool Archived { get; set; }
+
+
+        //navigation properties
+        public virtual Company Company { get; set; }
+        public virtual ProjectPriority ProjectPriority { get; set; }
+        public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
+        public virtual ICollection<Ticket> Tickets { get; set; } = new HashSet<Ticket>();
+
+        //public virtual BTUser Members { get; set; }
+       // public virtual Tickets  { get; set; }
+
+
+
+
+    }
+}
