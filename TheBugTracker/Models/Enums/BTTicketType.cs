8c1efd70b69fda4c8dfb35df5fc6e97354e336cd
wellsejoshua <wellsejoshua@outlook.com>
2022-07-27 14:41:35 -0500

    Added DataUtility class and required enums

---
+++ TheBugTracker/Models/Enums/BTTicketType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models.Enums
+{
+    public enum BTTicketType
+    {
+        NewDevelopment,
+        WorkTask,
+        Defect,
+        ChangeRequest,
+        Enhancement,
+        GeneralTask
+
+    }
+}
