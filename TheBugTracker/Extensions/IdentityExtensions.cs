ec867e6db34e0d9bdf192fa48fc7f56839bbf087
wellsejoshua <wellsejoshua@outlook.com>
2022-07-31 02:30:56 -0500

    Role Management: GET Method and ViewModel

---
+++ TheBugTracker/Extensions/IdentityExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static int? GetCompanyId(this IIdentity identity)
+        {
+            //Claims Identity implements the IIdentity
+            //identity is cast to claims identity to allow utilization of FindFirst() that allow
+            //you to look into the list of claims and find CompanyId
+            Claim claim = ((ClaimsIdentity)identity).FindFirst("CompanyId");
+            //ternary operator(if/else)
+            return (claim != null) ? int.Parse(claim.Value) : null;
+
+
+        }
+    }
+}
--- TheBugTracker/Services/BTRolesService.cs
+++ TheBugTracker/Services/BTRolesService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,33 +12,61 @@ namespace TheBugTracker.Services
 {
     public class BTRolesService : IBTRolesService
     {
+        #region Properties
         private readonly ApplicationDbContext _context;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly UserManager<BTUser> _userManager;
+        #endregion
 
+        #region Constructor
         public BTRolesService(ApplicationDbContext context,
-                                RoleManager<IdentityRole> roleManager,
-                                UserManager<BTUser> userManager)
+                        RoleManager<IdentityRole> roleManager,
+                        UserManager<BTUser> userManager)
         {
             _context = context;
             _roleManager = roleManager;
             _userManager = userManager;
-        }
+        } 
+        #endregion
 
+        #region Get Roles
+        public async Task<List<IdentityRole>> GetRolesAsync()
+        {
+            try
+            {
+                List<IdentityRole> result = new();
+
+                result = await _context.Roles.ToListAsync();
+
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Add User To Role
         public async Task<bool> AddUserToRoleAsync(BTUser user, string roleName)
         {
             bool result = (await _userManager.AddToRoleAsync(user, roleName)).Succeeded;
             return result;
         }
+        #endregion
 
+        #region Get Role Name By Id
         public async Task<string> GetRoleNameByIdAsync(string roleId)
         {
             IdentityRole role = _context.Roles.Find(roleId);
             string result = await _roleManager.GetRoleNameAsync(role);
             return result;
-            
-        }
 
+        }
+        #endregion
+
+        #region Get User Roles 
         public async Task<IEnumerable<string>> GetUserRolesAsync(BTUser user)
         {
             IEnumerable<string> result = await _userManager.GetRolesAsync(user);
@@ -45,14 +74,18 @@ namespace TheBugTracker.Services
 
 
         }
+        #endregion
 
+        #region Get Users In Role
         public async Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId)
         {
             List<BTUser> users = (await _userManager.GetUsersInRoleAsync(roleName)).ToList();
             List<BTUser> result = users.Where(u => u.CompanyId == companyId).ToList();
             return result;
         }
+        #endregion
 
+        #region Get Users Not In Role
         public async Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
         {
             List<string> userIds = (await _userManager.GetUsersInRoleAsync(roleName)).Select(u => u.Id).ToList();
@@ -61,25 +94,32 @@ namespace TheBugTracker.Services
             List<BTUser> result = roleUsers.Where(u => u.CompanyId == companyId).ToList();
 
             return result;
-            
-        }
 
+        }
+        #endregion
+
+        #region Is User In Role
         public async Task<bool> IsUserInRoleAsync(BTUser user, string roleName)
         {
             bool result = await _userManager.IsInRoleAsync(user, roleName);
             return result;
         }
+        #endregion
 
+        #region Remove User From Role
         public async Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName)
         {
             bool result = (await _userManager.RemoveFromRoleAsync(user, roleName)).Succeeded;
-                return result;
+            return result;
         }
+        #endregion
 
+        #region Remove User From Roles
         public async Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles)
         {
             bool result = (await _userManager.RemoveFromRolesAsync(user, roles)).Succeeded;
             return result;
-        }
+        } 
+        #endregion
     }
 }
