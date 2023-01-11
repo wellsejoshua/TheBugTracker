d6492ea6b24ee702109e0e35e50e6eb33cad7c27
wellsejoshua <codedbywells@gmail.com>
2023-01-11 07:15:39 -0600

    Trying to revert to finished registration

---
+++ TheBugTracker/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TheBugTracker.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string CompanyName { get; set; }
+        public string ProjectName { get; set; }
+        public string InvitorName { get; set; }
+        public string InviteeFirstName { get; set; }
+        public string InviteeLastName { get; set; }
+        public string Email { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
